354a08611b065361d8cca4a62a618fff96d28b09
ArKaRaShi <rnwz12345@gmail.com>
2025-09-18 15:24:27 +0700

    feat: enhance app module and main setup with global prefix and validation pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR, Reflector } from '@nestjs/core';
 
 import { AppConfigModule } from './config/app-config.module';
 import { AppController } from './app.controller';
+import { CommonSecondaryAdapterModules } from './common/adapters/secondary';
+import { UserModule } from './modules';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [AppConfigModule, ...CommonSecondaryAdapterModules, UserModule],
   controllers: [AppController],
+  providers: [
+    { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
+    Reflector,
+  ],
 })
 export class AppModule {}
