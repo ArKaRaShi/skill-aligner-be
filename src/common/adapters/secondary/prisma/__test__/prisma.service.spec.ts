b9187dadff96682bd01e56b67b63570c7211dfe3
ArKaRaShi <rnwz12345@gmail.com>
2025-09-18 15:24:38 +0700

    feat: add Prisma module and service with connection management

---
+++ src/common/adapters/secondary/prisma/__test__/prisma.service.spec.ts
@@ -0,0 +1,43 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma.service';
+
+describe('PrismaService', () => {
+  let service: PrismaService;
+  let connectSpy: jest.SpyInstance;
+  let disconnectSpy: jest.SpyInstance;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PrismaService],
+    }).compile();
+
+    service = module.get<PrismaService>(PrismaService);
+
+    connectSpy = jest.spyOn(service, '$connect').mockResolvedValue(undefined);
+    disconnectSpy = jest
+      .spyOn(service, '$disconnect')
+      .mockResolvedValue(undefined);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks(); // Restore original implementations
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('onModuleInit', () => {
+    it('should connect to the database (mocked)', async () => {
+      await service.onModuleInit();
+      expect(connectSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('onModuleDestroy', () => {
+    it('should disconnect from the database (mocked)', async () => {
+      await service.onModuleDestroy();
+      expect(disconnectSpy).toHaveBeenCalled();
+    });
+  });
+});
