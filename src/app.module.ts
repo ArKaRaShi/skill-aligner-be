e4928d15d2610983db7ea07ccbc4d17a2e1c2ebd
ArKaRaShi <rnwz12345@gmail.com>
2026-01-04 23:00:40 +0700

    refactor: remove query logging module and related components

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { APP_INTERCEPTOR, Reflector } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { CampusModule, CourseModule, QueryProcessorModule } from './modules';
 import { EvaluatorModule } from './modules/evaluator/evaluator.module';
-import { QueryLoggingModule } from './modules/query-logging/query-logging.module';
 import { PipelineModule } from './pipelines/pipeline.module';
 import { EmbeddingModule } from './shared/adapters/embedding/embedding.module';
 import { AppConfigModule } from './shared/kernel/config/app-config.module';
@@ -20,7 +19,6 @@ import { LoggerModule } from './shared/kernel/logger/logger.module';
     CampusModule,
     EmbeddingModule,
     QueryProcessorModule,
-    QueryLoggingModule,
     EvaluatorModule,
 
     PipelineModule,
